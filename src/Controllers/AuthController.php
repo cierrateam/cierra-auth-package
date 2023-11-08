a6b9ba594dcaec32f5ef0f1c068de1679cc4c018
alireza-cierra <alireza@cierra.de>
2023-11-08 08:32:18 +0100

    test

--- src/Controllers/AuthController.php
+++ src/Controllers/AuthController.php
@@ -86,7 +86,7 @@ class AuthController extends Controller
         );
 
         FacadesAuth::login($user);
-
+        session(['key11' => 'valueee']);
         return redirect(config('cierra-auth-package.redirect_after_login'));
     }
 }
