dfd40949825177999724f04003814fe27ab9fe71
alireza-cierra <alireza@cierra.de>
2023-11-08 08:54:46 +0100

    added web middleware

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 //routes with prefix cierra-auth
 
-Route::group(['prefix' => 'cierra-auth', 'middleware' => ['guest']], function () {
+Route::group(['prefix' => 'cierra-auth', 'middleware' => ['web', 'guest']], function () {
     Route::get('/login', [AuthController::class, 'login'])->name('cierra-auth.login');
     Route::get('/callback', [AuthController::class, 'callback'])->name('cierra-auth.callback');
 });
