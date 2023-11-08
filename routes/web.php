67cebef9472deffa814e6544db0253b64ec731fa
alireza-cierra <alireza@cierra.de>
2023-11-08 10:25:34 +0100

    fix

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,12 @@ use Illuminate\Support\Facades\Route;
 
 //routes with prefix cierra-auth
 
-Route::group(['prefix' => 'cierra-auth', 'middleware' => ['web', 'guest']], function () {
-    Route::get('/login', [AuthController::class, 'login'])->name('cierra-auth.login');
-    Route::get('/callback', [AuthController::class, 'callback'])->name('cierra-auth.callback');
+Route::group(['prefix' => 'cierra-auth', 'middleware' => ['web']], function () {
+    
+    Route::group(['middleware' => ['guest']], function () {
+        Route::get('/login', [AuthController::class, 'login'])->name('cierra-auth.login');
+        Route::get('/callback', [AuthController::class, 'callback'])->name('cierra-auth.callback');
+    });
+    
     Route::any('/logout', [AuthController::class, 'logout'])->name('cierra-auth.logout');
 });
