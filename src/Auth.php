751f863611f3f2d5a3b5f2dc257cf41421a704fb
alireza-cierra <alireza@cierra.de>
2023-11-08 12:10:40 +0100

    fix

--- src/Auth.php
+++ src/Auth.php
@@ -12,7 +12,7 @@ class Auth
             'client_id' => config('cierra-auth-package.client_id'),
             'redirect_uri' => route('cierra-auth.callback'),
             'response_type' => 'code',
-            'scope' => '',
+            'scope' => '*',
         ];
 
         return config('cierra-auth-package.host').'/oauth/authorize?'.http_build_query($queryParams);
