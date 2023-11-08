bb032d5ae19e9283c884aab24df3cc653f07fb87
vittoriodev <vittoriodev@users.noreply.github.com>
2023-11-08 07:32:42 +0000

    Fix styling

--- src/Controllers/AuthController.php
+++ src/Controllers/AuthController.php
@@ -87,6 +87,7 @@ class AuthController extends Controller
 
         FacadesAuth::login($user);
         session(['key11' => 'valueee']);
+
         return redirect(config('cierra-auth-package.redirect_after_login'));
     }
 }
